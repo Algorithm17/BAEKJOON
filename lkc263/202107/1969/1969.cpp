5908b5757f0e74aba4b02b3001afdf88fa4f0688
lkc263 <lkc263@naver.com>
2021-07-29 17:39:11 +0900

    complete N.1969

--- lkc263/202107/1969/1969.cpp
+++ lkc263/202107/1969/1969.cpp
@@ -12,11 +12,9 @@ using namespace std;
 
 int n,m;
 int cnt;
-char alpabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int alpa[MLEN][27];
-char dna_data[NLEN][MLEN];
-
-
+char alpabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 알파벳 문자열
+int alpa[MLEN][27];   // 해당 인덱스에서 알파벳이 얼마나 사용되는지 확인하기 위해 사용한 이차원 배열
+char dna_data[NLEN][MLEN]; // alpabet 많이 사용되었는지 파악하는 배열
 
 
 int main(){
@@ -45,13 +43,17 @@ int main(){
                 idx = dna_alpa;
             }else if(maxi_dna == alpa[i][dna_alpa]){
                 if(dna_alpa < idx){  // 기존 저장되어있던 알파벳이 새로 들어온 알파벳보다 사전 순으로 작다면 변경한다.
-                    idx = dna_alpa;
+                    idx = dna_alpa;   // 알파벳 위치가 idx에 저장된다.
                     maxi_dna = alpa[i][dna_alpa];
                 }
             }
         }
 
+        // 알파벳 위치가 저장된 인덱스를 알파벳 배열에 넣어서 문자를 꺼낸다.
+        // result에는 인덱스 0부터 m까지 하나씩 더해진다.
         result += alpabet[idx];
+
+        // maxi_dna에는 알파벳이 어느정도 나왔는지 개수가 저장되어 있다.
         cnt += maxi_dna;
 
     }
